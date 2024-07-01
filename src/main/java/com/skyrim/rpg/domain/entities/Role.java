e82b5ae94626cdb55876090bc22c1d16ee87d3c2
acn3to <costanetoow@gmail.com>
2024-06-30 21:56:28 -0300

    feat(entities): add Role and Skill entities

--- src/main/java/com/skyrim/rpg/domain/entities/Role.java
+++ src/main/java/com/skyrim/rpg/domain/entities/Role.java
@@ -1,4 +1,94 @@
 package com.skyrim.rpg.domain.entities;
 
-public class Role {
+import java.util.List;
+
+public abstract class Role {
+    private String name;
+    private String description;
+    private List<Skill> skills;
+    private int baseHealth;
+    private int baseMana;
+    private int baseAttack;
+    private int baseDefense;
+
+    public Role(String name, String description, List<Skill> skills, int baseHealth, int baseMana, int baseAttack, int baseDefense) {
+        this.name = name;
+        this.description = description;
+        this.skills = skills;
+        this.baseHealth = baseHealth;
+        this.baseMana = baseMana;
+        this.baseAttack = baseAttack;
+        this.baseDefense = baseDefense;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected String getDescription() {
+        return description;
+    }
+
+    protected void setDescription(String description) {
+        this.description = description;
+    }
+
+    protected List<Skill> getSkills() {
+        return skills;
+    }
+
+    protected void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    protected int getBaseHealth() {
+        return baseHealth;
+    }
+
+    protected void setBaseHealth(int baseHealth) {
+        this.baseHealth = baseHealth;
+    }
+
+    protected int getBaseMana() {
+        return baseMana;
+    }
+
+    protected void setBaseMana(int baseMana) {
+        this.baseMana = baseMana;
+    }
+
+    protected int getBaseAttack() {
+        return baseAttack;
+    }
+
+    protected void setBaseAttack(int baseAttack) {
+        this.baseAttack = baseAttack;
+    }
+
+    protected int getBaseDefense() {
+        return baseDefense;
+    }
+
+    protected void setBaseDefense(int baseDefense) {
+        this.baseDefense = baseDefense;
+    }
+
+    public abstract void useSkill(Skill skill);
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", skills=" + getSkills() +
+                ", baseHealth=" + getBaseHealth() +
+                ", baseMana=" + getBaseMana() +
+                ", baseAttack=" + getBaseAttack() +
+                ", baseDefense=" + getBaseDefense() +
+                '}';
+    }
 }
--- src/main/java/com/skyrim/rpg/domain/entities/Skill.java
+++ src/main/java/com/skyrim/rpg/domain/entities/Skill.java
@@ -1,4 +1,58 @@
 package com.skyrim.rpg.domain.entities;
 
 public class Skill {
+    private String name;
+    private String description;
+    private int manaCost;
+    private int power;
+
+    public Skill(String name, String description, int manaCost, int power) {
+        this.name = name;
+        this.description = description;
+        this.manaCost = manaCost;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getManaCost() {
+        return manaCost;
+    }
+
+    public void setManaCost(int manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", manaCost=" + getManaCost() +
+                ", power=" + getPower() +
+                '}';
+    }
 }
+
