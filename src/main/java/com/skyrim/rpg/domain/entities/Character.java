cbffa49ed2a89e872c6737f25a255ad13766e8db
acn3to <costanetoow@gmail.com>
2024-06-30 21:55:35 -0300

    feat(entities): add Character and Item entities

--- src/main/java/com/skyrim/rpg/domain/entities/Character.java
+++ src/main/java/com/skyrim/rpg/domain/entities/Character.java
@@ -1,4 +1,112 @@
 package com.skyrim.rpg.domain.entities;
 
 public class Character {
+    private String name;
+    private Role role;
+    private int level;
+    private int healthPoints;
+    private int manaPoints;
+    private int strength;
+    private int intelligence;
+    private int dexterity;
+    private int agility;
+
+    public Character(String name, Role role, int level, int healthPoints, int manaPoints, int strength, int intelligence, int dexterity, int agility) {
+        this.name = name;
+        this.role = role;
+        this.level = level;
+        this.healthPoints = healthPoints;
+        this.manaPoints = manaPoints;
+        this.strength = strength;
+        this.intelligence = intelligence;
+        this.dexterity = dexterity;
+        this.agility = agility;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected Role getRole() {
+        return role;
+    }
+
+    protected void setRole(Role role) {
+        this.role = role;
+    }
+
+    protected int getLevel() {
+        return level;
+    }
+
+    protected void setLevel(int level) {
+        this.level = level;
+    }
+
+    protected int getHealthPoints() {
+        return healthPoints;
+    }
+
+    protected void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    protected int getManaPoints() {
+        return manaPoints;
+    }
+
+    protected void setManaPoints(int manaPoints) {
+        this.manaPoints = manaPoints;
+    }
+
+    protected int getStrength() {
+        return strength;
+    }
+
+    protected void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    protected int getIntelligence() {
+        return intelligence;
+    }
+
+    protected void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    protected int getDexterity() {
+        return dexterity;
+    }
+
+    protected void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    protected int getAgility() {
+        return agility;
+    }
+
+    protected void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", role=" + role +
+                ", level=" + level +
+                ", healthPoints=" + healthPoints +
+                ", manaPoints=" + manaPoints +
+                ", strength=" + strength +
+                ", intelligence=" + intelligence +
+                ", dexterity=" + dexterity +
+                ", agility=" + agility +
+                '}';
+    }
 }
--- src/main/java/com/skyrim/rpg/domain/entities/Item.java
+++ src/main/java/com/skyrim/rpg/domain/entities/Item.java
@@ -1,4 +1,59 @@
 package com.skyrim.rpg.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Item {
+    private String name;
+    private String description;
+    private int value;
+    private List<String> effects;
+
+    public Item(String name, int value, List<String> effects) {
+        this.name = name;
+        this.value = value;
+        this.effects = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public List<String> getEffects() {
+        return effects;
+    }
+
+    public void setEffects(List<String> effects) {
+        this.effects = effects;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", value=" + getValue() +
+                ", effects=" + getEffects() +
+                '}';
+    }
 }
