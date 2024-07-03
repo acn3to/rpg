6d65dc018bdd21fa7ff06e0f52438b0591fed384
acn3to <costanetoow@gmail.com>
2024-07-02 21:01:10 -0300

    feat(controller): add GameController

--- src/main/java/com/skyrim/rpg/application/RpgApplication.java
+++ src/main/java/com/skyrim/rpg/application/RpgApplication.java
@@ -2,8 +2,10 @@ package com.skyrim.rpg.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.skyrim.rpg"})
 public class RpgApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,5 @@ public class RpgApplication {
 	}
 
 }
+
+
