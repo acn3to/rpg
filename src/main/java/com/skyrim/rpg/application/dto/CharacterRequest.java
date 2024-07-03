e031e85144e3455d8b57065af8fecf4399feb46a
acn3to <costanetoow@gmail.com>
2024-07-02 21:00:41 -0300

    feat(dto): add CharacterRequest dto

---
+++ src/main/java/com/skyrim/rpg/application/dto/CharacterRequest.java
@@ -0,0 +1,31 @@
+package com.skyrim.rpg.application.dto;
+
+public class CharacterRequest {
+    private String name;
+    private String description;
+    private String roleName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
